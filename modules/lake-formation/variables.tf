650f285929cd56455040d0225f9f3a4087b724e2
Fernando Sousa <fernando98sousa@gmail.com>
2026-08-29 20:30:50 -0300

    fix: cobre todos os modulos no CI e remove modulos fora de escopo
    - CI valida todos os modulos em modules/*, nao so 3 de 5
    - remove continue-on-error do terraform fmt -check
    - remove sagemaker e lake-formation: servicos nao constam
      na arquitetura de referencia definida pelo grupo

--- modules/lake-formation/variables.tf
+++
@@ -1,7 +0,0 @@
-# Variaveis para lake_formation
-
-variable "tags" {
-  description = "Tags padrao aplicadas aos recursos"
-  type        = map(string)
-  default     = {}
-}
--- modules/sagemaker/main.tf
+++
@@ -1,2 +0,0 @@
-# Recursos do modulo: sagemaker
-
